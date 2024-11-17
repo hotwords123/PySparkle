3910f96155904f76fd9ae65c7983e9a2828e679b
hotwords123 <hotwords123@gmail.com>
2024-11-17 20:33:54 +0800

    feat(driver): output lexer and parser results

--- driver.py
+++ driver.py
@@ -1,36 +1,80 @@
 import sys
+import json
+import yaml
 from antlr4 import *
-from grammar import PythonLexer, PythonParser, PythonErrorStrategy
+from antlr4.Token import CommonToken
+
+from grammar import PythonLexer, PythonParser, PythonParserVisitor, PythonErrorStrategy
+
+
+class PythonVisitor(PythonParserVisitor):
+    def visitChildren(self, node: RuleContext) -> dict:
+        children = [self.visit(child) for child in node.getChildren()]
+        return {get_rule_name(node): children}
+
+    def visitTerminal(self, node: TerminalNode) -> dict:
+        return {get_token_name(node.getSymbol()): node.getText()}
+
+
+def get_rule_name(rule: RuleContext) -> str:
+    return PythonParser.ruleNames[rule.getRuleIndex()]
+
+
+def get_token_name(token: CommonToken) -> str:
+    if token.type == Token.EOF:
+        return "EOF"
+    return PythonParser.symbolicNames[token.type]
 
 
 def main(argv):
-    input_stream = FileStream(argv[1])
+    args = parse_args(argv[1:])
+
+    out_file = open(args.output, "w", newline="") if args.output else sys.stdout
+
+    input_stream = FileStream(args.input)
     lexer = PythonLexer(input_stream)
     stream = CommonTokenStream(lexer)
-    parser = PythonParser(stream)
-    parser._errHandler = PythonErrorStrategy()
-    tree = parser.file_()
 
-    for token in stream.tokens:
-        print(token)
+    if args.mode == "lex":
+        stream.fill()
 
-    def indent(depth: int):
-        return "".join(["| ", ": "][i % 2] for i in range(depth))
-
-    def walk_tree(node, depth):
-        if isinstance(node, TerminalNode):
+        for token in stream.tokens:
+            token: CommonToken
+            token_type = get_token_name(token)
+            token_text = repr(token.text)
             print(
-                indent(depth)
-                + PythonParser.symbolicNames[node.getSymbol().type]
-                + ": "
-                + repr(node.getText())
+                f"{token.line}:{token.column}: {token_type} {token_text}",
+                file=out_file,
+                end="",
             )
-        elif isinstance(node, RuleContext):
-            print(indent(depth) + PythonParser.ruleNames[node.getRuleIndex()])
-            for child in node.getChildren():
-                walk_tree(child, depth + 1)
+            if token.channel != Token.DEFAULT_CHANNEL:
+                print(f" (channel={token.channel})", file=out_file, end="")
+            print(file=out_file)
 
-    walk_tree(tree, 0)
+    elif args.mode == "parse":
+        parser = PythonParser(stream)
+        parser._errHandler = PythonErrorStrategy()
+        tree = parser.file_()
+
+        visitor = PythonVisitor()
+        result = visitor.visit(tree)
+        yaml.dump(result, out_file, indent=2, sort_keys=False)
+
+    if args.output:
+        out_file.close()
+
+
+def parse_args(args: list[str] = None):
+    import argparse
+
+    # fmt: off
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mode", choices=["lex", "parse"], help="mode of operation")
+    parser.add_argument("input", help="input file")
+    parser.add_argument("-o", "--output", help="output file")
+    # fmt: on
+
+    return parser.parse_args(args)
 
 
 if __name__ == "__main__":
